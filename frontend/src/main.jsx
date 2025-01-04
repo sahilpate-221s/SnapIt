e092f6812ac0cc26d0f5e9b5fd3e793ac5a4938e
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-01-04 10:58:42 +0530

    added the login, signup page and the home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
@@ -17,6 +18,7 @@ createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <Toaster />
       </BrowserRouter>
     </Provider>
   </StrictMode>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://localhost:4000",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
