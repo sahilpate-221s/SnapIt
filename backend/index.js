17bc6a5b18f1822469d2a60064ada91ab9c5cf53
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-12-26 23:01:55 +0530

    added the deployed links

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: 'http://localhost:5173',
-		// origin: 'https://snapitapp1.netlify.app',
+		// origin: 'http://localhost:5173',
+		origin: 'https://snapitapp1.netlify.app',
 		credentials: true,
 	})
 );
